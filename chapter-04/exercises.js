9375e4e6c22e10f5a78186ac711b5b0af51a23e9
Leilay123 <leilayates2002@gmail.com>
2025-09-19 13:58:43 -0500

    chap4

--- chapter-04/exercises.js
+++ chapter-04/exercises.js
@@ -4,10 +4,47 @@
 
 const { resourceLimits } = require("worker_threads")
 
-// create array with contents of integers within given range
+//  create array with contents of integers within given range
 //  create array without contents when given same numbers as range
+// example - 
 
-function range() {
+function range(start, end, step = 1) {
+ /*
+ 
+ //base
+     if(array === 0){
+      return output
+    }
+
+  //recusrion
+
+  output.push(array - 1)
+
+ return range(array, output)
+
+ */
+
+ let result = [];
+
+ // if (1, 1)).to.deep.equal([])
+ if (start === end) {
+  return [];
+}
+
+ // Positive step, counting up
+ if (step > 0) {
+   for (let i = start; i <= end; i += step) {
+     result.push(i);
+   }
+ }
+ // Negative step, counting down
+ else if (step < 0) {
+   for (let i = start; i >= end; i += step) {
+     result.push(i);
+   }
+ }
+
+ return result;
 
 }
 
@@ -69,12 +106,12 @@ function reverseArrayInPlace(array) {
 // [1, 2,3 ] => {value: 1, value: 2, value: 3}
 
 function arrayToList(array) {
-  var list = null;
+  var rest = null;
 
   for (var i = array.length - 1; i >= 0; i--) {
-    list = {value: array[i], rest: list};
+    rest = {value: array[i], rest: rest};
   }
-  return list;
+  return rest;
 
 }
 
@@ -85,16 +122,17 @@ function arrayToList(array) {
 // turn object list into arrays
 // {value: 1, value: 2, value: 3} => [1, 2, 3 ]
 
-function listToArray(object) {
-  var newArr = [];
-
-  for (var key in object) {
-    if (object.hasOwnProperty(key)) {
-      newArr.push(object[key]);
+function listToArray(object, array=[]) {
+  // base
+    if(object.rest === null){
+      array.push(object.value)
+      return array
     }
-  }
-  
-  return newArr;
+
+  // recursions
+    array.push(object.value)
+
+  return listToArray(object.rest, array)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -112,9 +150,19 @@ function prepend(value, list) {
 ////////////////////////////////////////////////////////////////////////////////
 
 // Returns n-th value in given list
+// example ([123123], 4) => 2
 
-function nth(x, array) {
- 
+function nth(array, x) {
+ // if x isn't in array
+ if (!array) {
+  return undefined
+};
+
+ // base 
+ if (x === 0) {return array.value};
+
+ // recursive
+ return nth(array.rest, x - 1);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -132,7 +180,29 @@ function nth(x, array) {
 
 function deepEqual(x, y) {
   
-    
+  // check if x and y aren't objects
+    if(typeof x !== 'object' && typeof y !== 'object'){
+        return x === y
+    }
+  // ch4ck if x or y is not an object
+    if(typeof x !== 'object' || typeof y !== 'object'){
+      return false
+    }
+
+  // array of each input keys
+  var xKeys = Object.keys(x)
+  var yKeys = Object.keys(y)
+
+  if (xKeys.length !== yKeys.length){
+    return false
+  }
+// loop through to check if array keys match and values at keys matcj
+  for (var i = 0; i < xKeys.length; i++){
+    if(!yKeys.includes(xKeys[i]) || !deepEqual(x[xKeys[i]], y[xKeys[i]])){
+        return false
+    }
+  }
+return true
   }
 
 ////////////////////////////////////////////////////////////////////////////////
